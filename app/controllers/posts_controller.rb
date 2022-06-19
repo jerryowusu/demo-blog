6282497a4966a9f650136f7a194b3ef10e45b355
Jerry Owusu <jerryowusu6133@gmail.com>
2022-06-19 10:00:09 -0400

    Add delete method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,6 +36,13 @@ class PostsController < ApplicationController
         @post = Post.find(params[:id])
     end
 
+    def destroy
+      @post = Post.find(params[:id])
+      @post.destroy
+
+      redirect_to posts_path
+    end
+
     private  
 
     def post_params 
