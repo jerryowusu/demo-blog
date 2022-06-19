9cebfa7f2e8916b03b9f2e2436cb8a2a57f62e76
Jerry Owusu <jerryowusu6133@gmail.com>
2022-06-19 10:46:09 -0400

    Add comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_19_021501) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_19_144421) do
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
